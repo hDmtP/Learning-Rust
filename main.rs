9c92918a498ae428f078df78b898ee173d9116f9
Pritam Dhara <65482473+hDmtP@users.noreply.github.com>
2020-12-06 15:23:53 +0530

    guessing game using rand

--- main.rs
+++ main.rs
@@ -1,13 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Hello there, Guess a number");
-    println!("Please enter ur input");
+    println!("Guess the number");
+    let secrete_number = rand::thread_rng().gen_range(1,101);
+    println!("Please input  tyour guess: ");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Didn't get that");
-    println!(
-        "You have guessed: {}", guess
-    );
+    println!("You guessed: {} ", guess);
+    print!("Original number: {}", secrete_number);
 }
