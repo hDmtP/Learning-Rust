580974babaf21ad490fb4edd48c296c60ccd9764
Pritam Dhara <65482473+hDmtP@users.noreply.github.com>
2020-12-09 01:07:45 +0530

    Original guessing game in Rust

--- main.rs
+++ main.rs
@@ -1,14 +1,31 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number");
     let secrete_number = rand::thread_rng().gen_range(1,101);
-    println!("Please input  tyour guess: ");
+    loop{
     let mut guess = String::new();
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Didn't get that");
+    .read_line(&mut guess)
+    .expect("Didn't get that");
+    // let guess: u32 = guess.trim().parse().expect("Bylat! Type a number");
+    let guess: u32 = match guess.trim().parse() {
+        Ok(number) => number,
+        Err(_) => continue
+    };
+    
+    
+        println!("Please input  tyour guess: ");
+        match guess.cmp(&secrete_number){
+            Ordering::Greater => println!("BIg"),
+            Ordering::Less => println!("small"),
+            Ordering::Equal => {println!("You Win!");
+            break;
+        }
+    }
     println!("You guessed: {} ", guess);
-    print!("Original number: {}", secrete_number);
+}
+println!("Original number: {}", secrete_number);
 }
