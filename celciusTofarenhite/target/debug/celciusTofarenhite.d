a6cc382f0383619d79a1cd43b84cc404f1fbb91e
Pritam Dhara <pritamdhara914@gmail.com>
2020-12-15 11:53:37 +0530

    upto ownership

--- celciusTofarenhite/target/debug/celciusTofarenhite.d
+++
@@ -1,1 +0,0 @@
-C:\Users\user\Documents\RUST\ tut\celciusTofarenhite\target\debug\celciusTofarenhite.exe: C:\Users\user\Documents\RUST\ tut\celciusTofarenhite\src\main.rs
--- celciusTofarenhite/target/rls/debug/deps/celciusTofarenhite-ab09410a6690ed3a.d
+++
@@ -1,5 +0,0 @@
-c:\Users\user\Documents\RUST tut\celciusTofarenhite\target\rls\debug\deps\celciusTofarenhite-ab09410a6690ed3a.rmeta: src\main.rs
-
-c:\Users\user\Documents\RUST tut\celciusTofarenhite\target\rls\debug\deps\celciusTofarenhite-ab09410a6690ed3a.d: src\main.rs
-
-src\main.rs:
